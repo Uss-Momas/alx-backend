d875f4edb3f90dcfdd33bf3d29ecc48977c7167e
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-31 16:40:49 +0200

    Fix pycodestyle

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """app module"""
 from flask import Flask, render_template, request
-from flask_babel import  Babel, _
+from flask_babel import Babel, _
 
 
 class Config(object):
